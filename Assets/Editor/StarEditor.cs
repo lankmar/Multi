0d2fa2627eabb70f8cbd5f9dc6dfb14ea687ad25
lankmar <32324313+lankmar@users.noreply.github.com>
2023-12-25 14:27:39 +0300

    10
    10

---
+++ Assets/Editor/StarEditor.cs
@@ -0,0 +1,62 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Star)), CanEditMultipleObjects]
+public class StarEditor : Editor
+{
+    private SerializedProperty _center;
+    private SerializedProperty _points;
+    private SerializedProperty _frequency;
+    private void OnEnable()
+    {
+        _center = serializedObject.FindProperty("_center");
+        _points = serializedObject.FindProperty("_points");
+        _frequency = serializedObject.FindProperty("_frequency");
+    }
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_center);
+        EditorGUILayout.PropertyField(_points);
+        EditorGUILayout.IntSlider(_frequency, 1, 20);
+        var totalPoints = _frequency.intValue * _points.arraySize;
+        if (totalPoints < 3)
+        {
+            EditorGUILayout.HelpBox("At least three points are needed.",
+            MessageType.Warning);
+        }
+        else
+        {
+            EditorGUILayout.HelpBox(totalPoints + " points in total.",
+            MessageType.Info);
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void OnSceneGUI()
+    {
+
+        if (!(target is Star star))
+        {
+            return;
+        }
+        var starTransform = star.transform;
+        var angle = -360f / (star.frequency * star.points.Length);
+        for (var i = 0; i < star.points.Length; i++)
+        {
+            var rotation = Quaternion.Euler(0f, 0f, angle * i);
+            var oldPoint = starTransform.TransformPoint(rotation *
+            star.points[i].Position);
+            Vector3 snap = Vector3.one * 0.5f;
+            var newPoint = Handles.FreeMoveHandle(oldPoint, Quaternion.identity,
+            0.02f, snap, Handles.DotHandleCap);
+            if (oldPoint == newPoint)
+            {
+                continue;
+            }
+            star.points[i].Position = Quaternion.Inverse(rotation) *
+            starTransform.InverseTransformPoint(newPoint);
+            star.UpdateMesh();
+        }
+    }
+}
